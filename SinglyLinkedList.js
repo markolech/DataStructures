d8dc6d2e5d7c6ad6a4cb701342cf7b3c61d4e209
Mark <mark@olechconsulting.com>
2019-07-20 18:25:22 -0400

    SinglyLinkedList pop method

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -11,19 +11,44 @@ constructor(){
   this.tail = null
   this.length = 0
 }
-push(val){
-  let newNode = new Node(val)
-  if (this.head === null) {
-    this.head = newNode
-    this.tail = this.head
-  } else {
-    this.tail.next = newNode
-    this.tail = newNode
+  push(val){
+      let newNode = new Node(val)
+      if (!this.head) {
+        this.head = newNode
+        this.tail = this.head
+      } else {
+        this.tail.next = newNode
+        this.tail = newNode
+      }
+      this.length++
+      return this 
   }
-  this.length++
-  return this 
+  traverse(){
+    let current = this.head
+    while(current){
+      console.log("current: ",  current.val)
+      current = current.next
+    }
+  }
+  pop(){
+    if(!this.head) return undefined
+    let current = this.head
+    let newTail = current
+    while (current.next){
+      newTail = current
+      current = current.next
+    }
+      this.tail = newTail
+      this.tail.next = null
+      this.length--
+      if(this.length === 0) {
+        this.head = null
+        this.tail = null
+      }
+      return current
+  }
 }
-}
+
 
 let list = new SinglyLinkedList()
 list.push("Hello")
