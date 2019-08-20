08d39e54d422293c9a8dd3764975ecbd5031e483
Mark Olech <mark.olech@gmail.com>
2019-08-19 20:54:24 -0400

    DoublyLinkedList with push() and pop() methods

---
+++ DoublyLinkedList.js
@@ -0,0 +1,42 @@
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+    this.prev = null
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+  push(val) {
+    let newNode = new Node(val)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      newNode.prev = this.tail
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+  pop() {
+    if (!this.tail) return undefined
+    let poppedNode = this.tail
+    if (this.length === 1) {
+      this.head = null
+			this.tail = null
+    } else {
+      this.tail = this.tail.prev
+      this.tail.next = null
+			poppedNode.prev = null
+		}
+		this.length--
+		return poppedNode.val
+  }
+}
